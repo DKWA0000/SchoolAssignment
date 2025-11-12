204f382cb3e91fdd4bc67c6a924d7bb4e1187c8b
Begaiym Kalilova <easybeka@gmail.com>
2025-11-12 14:04:34 +0100

    minor changes

--- src/main/java/com/school/schoolSystem/repository/StudentRepository.java
+++ src/main/java/com/school/schoolSystem/repository/StudentRepository.java
@@ -1,56 +1,10 @@
 package com.school.schoolSystem.repository;
 
-import com.school.schoolSystem.dto.StudentResponseDTO;
 import com.school.schoolSystem.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-public class StudentRepository {
-
-    private List<Student> students;
-
-    public StudentRepository(List<Student> students) {
-        this.students = students;
-    }
-
-    public List<StudentResponseDTO> findAll(){
-        return students.stream().map(
-                s -> new StudentResponseDTO(
-                        s.getId(),       /*ID*/
-                        s.getName(),     /*name*/
-                        s.getAge(),      /*age*/
-                        s.getEmail()     /*email*/
-                )
-        ).toList();
-    }
-
-    public List<StudentResponseDTO> findByName(String name){
-        return students.stream().filter(s -> s.equals(name)).map(s ->
-                new StudentResponseDTO(
-                    s.getId(),
-                    s.getName(),
-                    s.getAge(),
-                    s.getEmail()
-                )).toList();
-    }
-
-    public List<StudentResponseDTO> findByAge(int age){
-        return students.stream().filter(s -> s.getAge() == age).map(s ->
-                new StudentResponseDTO(
-                    s.getId(),
-                    s.getName(),
-                    s.getAge(),
-                    s.getEmail()
-                )).toList();
-    }
-
-    public boolean deleteStudent(int id){
-        long n = students.stream().filter(s -> s.getId() == id).count();
-        students = students.stream().filter(s -> s.getId() != id).toList();
-        return n > 0;
-    }
-
-
 }
+
--- src/main/java/com/school/schoolSystem/repository/StudentRepositoryOld.java
+++ src/main/java/com/school/schoolSystem/repository/StudentRepositoryOld.java
@@ -1,5 +1,6 @@
 package com.school.schoolSystem.repository;
 
+import com.school.schoolSystem.dto.StudentResponseDTO;
 import com.school.schoolSystem.model.Student;
 import org.springframework.stereotype.Repository;
 
@@ -14,4 +15,40 @@ public class StudentRepositoryOld {
         this.students = students;
     }
 
+    public List<StudentResponseDTO> findAll(){
+        return students.stream().map(
+                s -> new StudentResponseDTO(
+                        s.getId(),       /*ID*/
+                        s.getName(),     /*name*/
+                        s.getAge(),      /*age*/
+                        s.getEmail()     /*email*/
+                )
+        ).toList();
+    }
+
+    public List<StudentResponseDTO> findByName(String name){
+        return students.stream().filter(s -> s.equals(name)).map(s ->
+                new StudentResponseDTO(
+                        s.getId(),
+                        s.getName(),
+                        s.getAge(),
+                        s.getEmail()
+                )).toList();
+    }
+
+    public List<StudentResponseDTO> findByAge(int age){
+        return students.stream().filter(s -> s.getAge() == age).map(s ->
+                new StudentResponseDTO(
+                        s.getId(),
+                        s.getName(),
+                        s.getAge(),
+                        s.getEmail()
+                )).toList();
+    }
+
+    public boolean deleteStudent(int id){
+        long n = students.stream().filter(s -> s.getId() == id).count();
+        students = students.stream().filter(s -> s.getId() != id).toList();
+        return n > 0;
+    }
 }
