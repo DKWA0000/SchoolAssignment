8db94606f7570417d4df3961bb8b9fb03a7857eb
Begaiym Kalilova <easybeka@gmail.com>
2025-11-13 12:34:07 +0100

    second change with title& teacher

--- src/main/java/com/school/schoolSystem/repository/CourseRepository.java
+++ src/main/java/com/school/schoolSystem/repository/CourseRepository.java
@@ -2,6 +2,8 @@ package com.school.schoolSystem.repository;
 
 import com.school.schoolSystem.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,12 @@ import java.util.Optional;
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Integer> {
 
+    @Query("SELECT c FROM Course c WHERE LOWER(c.title) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Course> findAllByTitle(@Param("keyword") String title);
+
     Optional<Course> findByTitle(String title);
     List<Course> findAll();
-    List<Course> findAllByTeacher(String teacher);
+
+    @Query("SELECT c FROM Course c WHERE LOWER(c.teacher) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Course> findAllByTeacher(@Param("keyword") String teacher);
 }
